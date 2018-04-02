89513eff0e2588f54a73627fb7c8ea280e06d2e4
Saddam Bilalov <saddam.bilalov@outlook.com>
2018-04-02 22:28:05 +0200

    MogoDB_HW_2_3 solved

---
+++ Models/Grades.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDB.Models
+{
+    public class Grades
+    {
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("student_id")]
+        public int StudentId { get; set; }
+
+        [BsonElement("type")]
+        public string Type { get; set; }
+
+        [BsonElement("score")]
+        public double Score { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using MongoDB.Driver;
+using MongoDB.Models;
+
+namespace MongoDB
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            const string connectionString = "mongodb://localhost:27017";
+            var client = new MongoClient(connectionString);
+
+            var db = client.GetDatabase("students");
+            var collection = db.GetCollection<Grades>("grades");
+
+            var elementsToRemove = collection.Find(x => x.Type.Equals("homework"))
+                .ToList()
+                .GroupBy(x =>  x.StudentId)
+                .Select(grp => new
+                {
+                    grp.Key,
+                    Result = grp.OrderBy(x => x.Score).FirstOrDefault()
+                }).ToList();
+
+            foreach (var element in elementsToRemove)
+            {
+                collection.DeleteOne(x => x.Id == element.Result.Id);
+            }
+        }
+    }
+}
